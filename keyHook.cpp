6f256611b8472bb5268fb5d4d358181fed04d759
Jasper Lorenz <jasper@lorenzmail.de>
2024-02-07 20:33:30 +0100

    Initial commit

---
+++ keyHook.cpp
@@ -0,0 +1,88 @@
+#include <windows.h>
+#include <stdio.h>
+
+/*
+ * This is a quote from the windows header file:
+ * VK_0 - VK_9 are the same as ASCII '0' - '9' (0x30 - 0x39)
+ * 0x3A - 0x40 : unassigned
+ * VK_A - VK_Z are the same as ASCII 'A' - 'Z' (0x41 - 0x5A)
+ */
+bool isAlphaNumeric(int vkCode) {
+    return (0x30 <= vkCode && vkCode <= 0x39) || (vkCode >= 0x41 && vkCode <= 0x5A);
+}
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode != HC_ACTION) {
+        return CallNextHookEx(NULL, nCode, wParam, lParam);
+    }
+
+    char *prefix = (wParam == WM_KEYDOWN) ? "Press-" : "Release-";
+
+    KBDLLHOOKSTRUCT* pKbStruct = (KBDLLHOOKSTRUCT*)lParam;
+    // Check for Controll Keys
+    switch (pKbStruct->vkCode) {
+        case VK_TAB:
+            printf("%sTab\n", prefix);
+            break;
+        case VK_CAPITAL:
+            printf("%sCaps\n", prefix);
+            break;
+        case VK_LSHIFT:
+        case VK_RSHIFT:
+        case VK_SHIFT:
+        case MK_SHIFT:
+            printf("%sShift\n", prefix);
+            break;
+        case VK_LCONTROL:
+        case VK_RCONTROL:
+        case VK_CONTROL:
+        case MK_CONTROL:
+            printf("%sCtrl\n", prefix);
+            break;
+        // Function keys F1 - F12
+        case VK_F1:
+        case VK_F2:
+        case VK_F3:
+            printf("%sF%d\n", prefix, pKbStruct->vkCode - VK_F1 + 1);
+            break;
+        case VK_ESCAPE:
+            printf("%sEsc\n", prefix);
+            break;
+        case VK_SPACE:
+            printf("%sSpace\n", prefix);
+            break;
+        default:
+            // Alphanumerical keys
+            if (isAlphaNumeric(pKbStruct->vkCode)) {
+                printf("%s%c\n", prefix, pKbStruct->vkCode);
+            }
+    }
+
+    // Flush stdout so if run as subprocess from main.py, the output is immediately available
+    fflush(stdout);
+
+    // Call the next hook in the chain
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+
+int main() {
+    // Set up low-level keyboard hook
+    HHOOK hook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(NULL), 0);
+
+    if (hook == NULL) {
+        // Handle hook setup failure
+        printf("Could not register Key Hook");
+        return 1;
+    }
+    // Message loop to keep the program running
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) != 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    // Unhook the keyboard hook before exiting
+    UnhookWindowsHookEx(hook);
+    return 0;
+}
---
+++ main.py
@@ -0,0 +1,117 @@
+import tkinter as tk
+import subprocess
+import threading
+
+# Define the command to run the keyHook.exe
+CMD = r".\\keyHook.exe"
+
+# Define the action types for key events received from the keyHook.exe
+ACTION_TYPE_PRESS = "Press"
+ACTION_TYPE_RELEASE = "Release"
+
+FUNCTIONAL_KEYS =  ["F1", "Esc"]
+
+
+class KeyHookThread(threading.Thread):
+    def __init__(self, callback):
+        super().__init__()
+        self.alive = True
+        self.callback = callback
+        self.proc = None
+
+    def run(self):
+        for action in self.iter_stdout():
+            action_type, key = action.split("-")
+            self.callback(action_type, key)
+
+    def stop(self):
+        self.alive = False
+        self.proc.kill()
+
+    def iter_stdout(self):   
+        self.proc = subprocess.Popen(
+                CMD,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+        for stdout_line in iter(self.proc.stdout.readline, ""):
+            if not self.alive:
+                print(stdout_line)
+                break
+            if stdout_line != b'':
+                yield stdout_line.decode("utf-8").strip()
+
+
+class KeyboardApp:
+    def __init__(self, root):
+        self.BACKGROUND_COLOR = "dark grey"
+        self.HIGHLIGHT_COLOR = "light blue"
+        self.root = root
+        self.root.geometry("600x300")
+        self.root.title("Keyboard Visualizer")
+        self.key_listner = KeyHookThread(self.on_key_action)
+        self.key_listner.start()
+
+        # Make background transparent
+        self.root.attributes('-alpha', 0.9)
+        self.root.configure(bg=self.BACKGROUND_COLOR)
+
+        # Define the updated keyboard layout
+        self.keyboard_layout = [
+            ["Tab", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+            ["Caps", "Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P"],
+            ["Shift", "A", "S", "D", "F", "G", "H", "J", "K", "L"],
+            ["Ctrl", "Y", "X", "C", "V", "Space", "B", "N", "M"]
+        ]
+
+        self.keys = [key for row in self.keyboard_layout for key in row]
+    
+        # Create a canvas to hold the key buttons
+        self.button_canvas = tk.Canvas(root, bg='white', highlightthickness=0)
+        self.button_canvas.pack(expand=True, fill='both')
+
+        # Create a dictionary to store the key buttons
+        self.key_buttons = {}
+        max_buttons = max(len(row) for row in self.keyboard_layout)
+    
+        # Create and place the key buttons on the canvas with resizable options
+        for row_idx, row in enumerate(self.keyboard_layout):
+            for col_idx, key in enumerate(row):
+                button = tk.Button(self.button_canvas, bg=self.BACKGROUND_COLOR, text=key,
+                                   command=lambda k=key: self.highlight_key(k))
+                button.grid(row=row_idx, column=col_idx, sticky="nsew", padx=1, pady=1)
+                # Make buttons resizable
+                self.button_canvas.columnconfigure(col_idx, weight=1, uniform="group1")
+                self.button_canvas.rowconfigure(row_idx, weight=1)
+                self.key_buttons[key] = button
+
+        # Override default controll key behaviour
+        self.root.bind("<Tab>", lambda _: "break")
+        self.root.bind("<Caps_Lock>", lambda _: "break")
+        self.root.bind("<Control_L>", lambda _: "break")
+        self.root.bind("<Shift_L>", lambda _: "break")
+
+    def quit(self):
+        # Call to close subprocess so the thread running it can join
+        self.key_listner.stop()
+        self.root.quit()
+        exit(0)
+
+    def on_key_action(self, action_type, pressed_key):
+        if pressed_key in self.keys:
+            self.key_buttons[pressed_key].config(bg=self.HIGHLIGHT_COLOR if action_type == ACTION_TYPE_PRESS else self.BACKGROUND_COLOR)
+        elif pressed_key in FUNCTIONAL_KEYS:
+            if (action_type == ACTION_TYPE_PRESS):
+                match pressed_key:
+                    case "F1":
+                        self.root.iconify() if self.root.state() == "normal" else self.root.deiconify()
+                    case "Esc":
+                        self.quit()
+                    case unknwon:
+                        pass
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = KeyboardApp(root)
+    root.mainloop()
+
